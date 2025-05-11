82106b21250f6c8e7fccd69d838640fc80ba23e6
José Chociai <jose.lucas@sousmile.com>
2025-05-10 22:03:15 -0300

    fix POST /reset resource

--- src/reset/reset.service.ts
+++ src/reset/reset.service.ts
@@ -18,30 +18,25 @@ export class ResetService {
     await this.accountsService.deleteAll();
     await this.eventsService.deleteAll();
 
-    await Promise.all([
-      this.accountsService.create(
-        new CreateAccountDto({
-          id: 100,
-          name: 'Account 01',
-          nationalRegister: '0000000000',
-        }),
-      ),
-      this.accountsService.create(
-        new CreateAccountDto({
-          id: 300,
-          name: 'Account 02',
-          nationalRegister: '0000000000',
-        }),
-      ),
-    ]);
-
-    await Promise.all([
-      this.balancesService.create(
-        new CreateBalanceDto({ accountId: 100, currentBalance: 0 }),
-      ),
-      this.balancesService.create(
-        new CreateBalanceDto({ accountId: 300, currentBalance: 0 }),
-      ),
-    ]);
+    await this.accountsService.create(
+      new CreateAccountDto({
+        id: 100,
+        name: 'Account 01',
+        nationalRegister: '0000000000',
+      }),
+    );
+    await this.accountsService.create(
+      new CreateAccountDto({
+        id: 300,
+        name: 'Account 02',
+        nationalRegister: '0000000000',
+      }),
+    );
+    await this.balancesService.create(
+      new CreateBalanceDto({ accountId: 100, currentBalance: 0 }),
+    );
+    await this.balancesService.create(
+      new CreateBalanceDto({ accountId: 300, currentBalance: 0 }),
+    );
   }
 }
